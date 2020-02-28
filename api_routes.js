f5e8c591235f8dce960fc603f9e0b4c5ff7e1601
Caleb Vincent <vincent_806@yahoo.com>
2020-02-27 23:07:43 -0600

    Setup api routes

--- api_routes.js
+++ api_routes.js
@@ -2,25 +2,50 @@ const db = require("./exerciseModel");
 
 module.exports = function (app) {
 
-    // creates an empty table for exercises to be added to
-
-
-    app.post("/api/exercise", async ({ body }, res) => {
-        const request = await db.create(body)
-        res.send(request)
-    });
+    app.post("api/exercise/", function (req, res) {
+        db.Workout.create({ exercise: req.body }).then(function (dbWorkout) {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err)
+        })
+    })
 
     // route for adding exercise
-    app.put("/api/exercise/:id", async (req, res) => {
-        const request = await db.findOneAndUpdate({_id: req.params.id}, {
-            $push: {exercises: req.body}
+    app.put("/api/exercise/:id", function (req, res) {
+        var query = { _id: req.params.id };
+        db.Workout.findOneAndUpdate(query, {
+            $push: { exercises: [req.body] }
+        }, function (err, dbWorkout) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.send(dbWorkout)
+            }
         })
-        res.send(request)
-    });
+    })
 
-    app.get("/api/exercise", async ({body}, res) => {
-        const request = await db.find({})
-        res.send(request)
-    });
+    app.get("/api/exercise", function (req, res) {
+        db.Workout.find({}).then(function (dbWorkout) {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        })
+    })
+
+    app.get("api/exercise/range", (req, res) => {
+        db.Workout.find({}).then(function (dbWorkout) {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        })
+    })
+
+    // app.get("/api/exercise/:id", function (req, res) {
+    //     const request = req.params.id
+    //     db.Workout.findById(request, function (err, dbWorkout) {
+    //     }).catch(err => {
+    //         res.json(err);
+    //     })
+    // })
 
 }
